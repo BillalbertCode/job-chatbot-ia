d5deb3ca66d45c41be883d84967a396055a08f38
BillalbertCode <BillalbertCode@gmail.com>
2024-10-11 20:18:08 -0400

    Bug en el tipyng del texto
    El texto salia doble y el bucle no parada por las multiples llamadas asyncronas, agragamos una validacion extra de carga para solucionarlo

--- src/components/utils/TypeWriter.tsx
+++ src/components/utils/TypeWriter.tsx
@@ -14,12 +14,13 @@ interface TypeWriterProps extends HTMLAttributes<HTMLParagraphElement> {
 const TypeWriter: React.FC<TypeWriterProps> = ({ text, speed = 50, className, ...props }) => {
   const [displayedText, setDisplayedText] = useState<string>("");  // Text currenty displayed
 
-  const [load, setLoad] = useState<boolean>(true) // state of load component for writing
+  const [load, setLoad] = useState<boolean>(false) // state of load component for writing
 
   const timer = (ms: number) => new Promise(res => setTimeout(res, ms)) // timer for function
 
   // Function Typing letter of the text, parameter is the number of init Typing
   async function writer(index: number) {
+    setLoad(true)
     let currentIndex: number = index // Index of text
     for (currentIndex; currentIndex < text.length; currentIndex++) {
       setDisplayedText((prev) => prev + text[currentIndex]) // add letter
@@ -31,11 +32,10 @@ const TypeWriter: React.FC<TypeWriterProps> = ({ text, speed = 50, className, ..
 
   useEffect(() => {
     // If new text appears, update it
-    if (text.length > displayedText.length) {
-      setLoad(true) // culminated writing
+    if (text.length > displayedText.length && !load) {
       writer(displayedText.length) // writer displayedText.lenght as index init 
     }
-  }, [text, displayedText]);
+  }, [text, displayedText, load]);
 
   return <p {...props} className={className}>{displayedText}<span className={`${load && animation.typing}`}></span></p>;  // Reder of text is progress
 };
