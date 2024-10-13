2e2a256a123aac0d6fc7640b0a17717a4177d265
BillalbertCode <BillalbertCode@gmail.com>
2024-10-13 11:00:02 -0400

    Hero section Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,18 @@ import stylesCss from "@/styles/figure.module.css"
 export default function Home() {
   return (
     <>
-      <section id="Hero" className="mb-16 text-wrap w-9/12 max-w-4xl">
-        <div className="flex items-center justify-center relative">
-          <h1 className="textbg1 tracking-wide text-center text-6xl font-extrabold mb-2">
-            Job Chatbot <span className="relative">IA</span>
+      <section id="Hero" className="mb-16 text-wrap">
+        <div className="flex items-center justify-center flex-col sm:flex-row">
+          <h1 className="textbg1 tracking-wide text-center text-6xl font-extrabold mb-0 sm:mb-2 ">
+            Job Chatbot <span>IA</span>
           </h1>
-          <div className="relative p-4 h-16">
-            <SiGooglegemini className={`${stylesCss.starIA} absolute bottom-3 left-0 text-xl`} />
-            <SiGooglegemini className={`${stylesCss.starIA} absolute bottom-50 left-50 text-xl`} />
-            <SiGooglegemini className={`${stylesCss.starIA} absolute top-10 left-100 text-xl`} />
+          <div className="relative flex p-4 h-16">
+            <SiGooglegemini className={`${stylesCss.starIA} sm:absolute bottom-3 left-0 text-xl`} />
+            <SiGooglegemini className={`${stylesCss.starIA} sm:absolute sm:text-xl`} />
+            <SiGooglegemini className={`${stylesCss.starIA} sm:absolute top-10 text-xl`} />
           </div>
         </div>
-        <article className="relative">
+        <article className="max-w-4xl relative">
           <div className={`absolute top-100 right-0 z-n1 rounded h-24 w-32 ${stylesCss.bgFigure2}`}>
           </div>
           <p className="font-medium text-xl text-gray-600 text-center">
