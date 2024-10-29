43ad550933f0e5328598c009d26e79267eeb63ca
BillalbertCode <BillalbertCode@gmail.com>
2024-10-29 18:43:15 -0400

    Select template logic

--- src/components/tools/generateCVTool/GenerateCVTool.tsx
+++ src/components/tools/generateCVTool/GenerateCVTool.tsx
@@ -1,19 +1,42 @@
 // Using for generation CV with user info
 "use client"
+import React, { useState } from "react"
+// Components
 import LayoutTool from "../LayoutTool"
-import React from "react"
+// Helpers
 import downloadPDF from "./helper/downloadPDF"
 
 const GenerateCVTool = (user: Object) => {
 
+    //  Templates options
+    const templates = [
+        "Harvard",
+        "Example"
+    ]
+
+    // capture select template
+    const [selectTemplate, setSelectTemplate] = useState<string>(templates[0])
+
+    const handleSelectTemplate = (event : React.ChangeEvent<HTMLInputElement>) => {
+        setSelectTemplate(event.target.value);
+    };
+
+    //  Template container ref
     const templateCVRef: React.RefObject<HTMLDivElement> = React.createRef()
-
-
 
     return (
         <LayoutTool toolName="Generate CV">
+            <div>
+                <input type="radio" name="template" value={templates[0]} checked={selectTemplate === templates[0]} onChange={handleSelectTemplate} />
+
+                <input type="radio" name="template" value={templates[1]} checked={selectTemplate === templates[1]} onChange={handleSelectTemplate} />
+
+                <label htmlFor=""></label>
+                <label htmlFor=""></label>
+            </div>
             <div ref={templateCVRef} >
-                <h1>Hola como estas?</h1>
+                <h2>{selectTemplate}</h2>
+
             </div>
             <button
                 className="hover:bg-red-600"
